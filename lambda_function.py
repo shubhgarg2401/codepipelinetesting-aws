51410c1d3662ef9024f3cd6049d445d51c83ad5d
Shubh Garg <53488130+shubhgarg2401@users.noreply.github.com>
2022-02-08 13:31:44 +0530

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,7 @@
+import json
+import boto3
+
+ec2 = boto3.client('ec2')
+def lambda_handler(event, context):
+  response = ec2.describe_availability_zones()
+  return {"statuscode":200,"body": json.dumps(response)}
